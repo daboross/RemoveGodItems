a1400feaa7de2f3b99caeffc9958da3edb025284
Dabo Ross <daboross@daboross.net>
2013-07-07 23:38:31 -0700

    Fixed

--- src/main/java/net/daboross/bukkitdev/goditemfixer/GodItemFixer.java
+++ src/main/java/net/daboross/bukkitdev/goditemfixer/GodItemFixer.java
@@ -5,7 +5,6 @@
  */
 package net.daboross.bukkitdev.goditemfixer;
 
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,12 +12,12 @@ import org.bukkit.plugin.java.JavaPlugin;
  *
  * @author daboross
  */
-public class GodItemFixer extends JavaPlugin implements Listener {
+public class GodItemFixer extends JavaPlugin {
 
     @Override
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(this, this);
+        pm.registerEvents(new GodItemFixerListener(this), this);
     }
 
     @Override
