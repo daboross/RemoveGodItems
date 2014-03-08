8ca034cec20adf3657d1b624fafcc180807b29bc
Dabo Ross <daboross@daboross.net>
2014-03-08 14:49:21 -0800

    Start adding blanket listener

---
+++ src/main/java/net/daboross/bukkitdev/removegoditems/BlanketOnCreativeListener.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2013 Dabo Ross <http://www.daboross.net/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.daboross.bukkitdev.removegoditems;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCreativeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+public class BlanketOnCreativeListener implements Listener {
+
+    @EventHandler
+    public void onCreative(InventoryCreativeEvent evt) {
+        ItemStack itemStack = evt.getCurrentItem();
+        ItemStack newStack = new ItemStack(itemStack.getType(), itemStack.getAmount());
+        evt.setCurrentItem(newStack);
+    }
+
+    public void unregister() {
+        InventoryCreativeEvent.getHandlerList().unregister(this);
+    }
+
+    public void register(Plugin plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+}
